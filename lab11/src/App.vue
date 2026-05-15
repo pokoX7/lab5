620b2973450fda56c8d2f02e593e060a1ebdb713
pokoX7 <raldugina123@yandex.ru>
2026-05-16 00:51:47 +0300

    Add files via upload

---
+++ lab11/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Clicker
+    v-for="i in 100"
+    :key="i"
+    :amount="1"
+    @button-clicked="handleClick(i)"
+  >
+    <button>Click me</button>
+  </Clicker>
+</template>
+
+<script setup>
+import Clicker from './components/Clicker.vue'
+
+function handleClick(n) {
+  alert('Clicker ' + n + ' clicked!')
+}
+</script>
---
+++ lab11/src/components/Clicker.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div @click="increment">
+      <slot></slot>
+    </div>
+    <p>Score: {{ score }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  amount: {
+    type: Number,
+    default: 1
+  }
+})
+
+const emit = defineEmits(['button-clicked'])
+
+const score = ref(0)
+
+function increment() {
+  score.value += props.amount
+  emit('button-clicked')
+}
+</script>
